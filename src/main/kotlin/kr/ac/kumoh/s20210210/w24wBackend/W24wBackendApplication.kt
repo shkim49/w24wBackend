af22c8fea99f913a556fb388c8e69d58fbae02b5
shkim49 <lojeve62@gmail.com>
2024-12-13 16:24:41 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210210/w24wBackend/W24wBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210210.w24wBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24wBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24wBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210210/w24wBackend/W24wBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210210.w24wBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24wBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
